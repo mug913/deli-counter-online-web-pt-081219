d1dd7f1c8a9706b8a6f84a046a7102f9ec10b4e3
Brad Alexander <brad.alexander913@gmail.com>
2019-08-15 00:37:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,3 +6,11 @@ def line(spot)
     puts = "The line is currently empty."
   end
 end
+
+def take_a_number(array, new_name)
+  array << new_name
+  puts "#{new_name}, Your spot is #{array[-1] + 1}."
+end
+
+def now_serving(array)
+  
