93f3ca11b387c1a8fd1afbe1c27f8ab0955c171d
Brad Alexander <brad.alexander913@gmail.com>
2019-08-15 01:08:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
      puts "The line is currently empty."
   else 
     array.each_with_index do |name, index|
-      spot = index + 1
+      spot = "#{index + 1}."
       line.push spot
       line.push name
     end
